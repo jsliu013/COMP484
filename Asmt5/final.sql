f5443d2b993d76656e838cb6b1c174a636373a3a
Paladinv82 <ian.postel.476@my.csun.edu>
2018-05-07 17:41:42 -0700

    new file: final.sql; open folder for new asmt5

---
+++ Asmt5/final.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS finaldb;
+
+CREATE DATABASE finaldb;
+
+USE finaldb;
+
+CREATE TABLE auth
+(
+   userid int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   username varchar(60),
+   password varchar(60),
+   firstName varchar(60),
+   lastName varchar(60),
+   email varchar(60),
+   phone varchar(60)
+);
+
+
+INSERT INTO auth (username, password, firstName, lastName, email, phone) VALUES ('lonestar1','MayThSchtzBeWithU','Lone','Star','lonestar1@schwartzmail.com','2173550320');
+INSERT INTO auth (username, password, firstName, lastName, email, phone) VALUES ('KRoland','12345','King','Roland','palace@druidia.pl','8003784342');
+INSERT INTO auth (username, password, firstName, lastName, email, phone) VALUES ('PrezScroobLuggage','12345','President','Scroob','scroob@spaceballs.ge','8005882300');
