5696e9e5989bf32725ef9c3ff98342297e71b86f
AH-C <ian.postel.476@my.csun.edu>
2018-05-15 19:45:48 -0700

    this might work

--- Asmt5/signup.php
+++ Asmt5/signup.php
@@ -17,7 +17,7 @@ if(!($database=mysqli_connect($servername,$username,$pass)))
 if(!mysqli_select_db($database,$dbname))
   die("Could not open finaldb");
   
-if(!$result=mysqli_query($database,"INSERT INTO auth (username, password, firstName, lastName, email, phone) VALUES ($_POST['username'], $_POST['password'], $_POST['firstname'], $_POST['lastname'], $_POST['email'], $_POST['phone'])")
+if(!($result=mysqli_query($database,"INSERT INTO auth (username, password, firstName, lastName, email, phone) VALUES ($_POST[username], $_POST[password], $_POST[firstname], $_POST[lastname], $_POST[email], $_POST[phone])")))
   die("Error occurred trying to register you.");
   
 print("You are now registered.");
@@ -29,12 +29,12 @@ mysqli_close($database);
 function showTable(){
   
   if(!$tables=mysqli_query($database,
-    "SELECT firstName, lastName FROM auth")
+    "SELECT firstName, lastName FROM auth"))
     die("Error occurred trying to show user table.");
     
   print("<table><thead><th>First Name</th><th>Last Name</th></thead><tbody>");
   while($row=mysqli_fetch_assoc($tables)){
-    print("<tr><td>$row['firstName']</td><td>$row['lastName']</td></tr>");
+    print("<tr><td>$row[firstName]</td><td>$row[lastName]</td></tr>");
   }
   print("</tbody></table>");
 }
