330f5f9f3304304a1ed0dfdaf34827a0deac3d85
Joshua Liu <joshliu013@gmail.com>
2018-05-15 19:16:56 -0700

    Php first commit
    Recoded home.js
    Preliminary completion of login.php

--- Asmt5/home.js
+++ Asmt5/home.js
@@ -1,6 +1,15 @@
+window.addEventListener("load",start);
+
+function start(){
+	var pass1= document.getElementById('password1');
+	var pass2= document.getElementById('password2');
+	
+	pass1.addEventListener("keyup", passMatch);
+	pass2.addEventListener("keyup", passMatch);
+}
 
 function passMatch(){
-	var pass1= document.getElementById('password1');
+    var pass1= document.getElementById('password1');
 	var pass2= document.getElementById('password2');
 	var issue= document.getElementById("nomatch");
 	
